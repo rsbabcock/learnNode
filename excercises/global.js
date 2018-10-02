9f292429e2b15a8bb17cb8e7bee18948d8696bf1
rsbabcock <rachael.s.babcock@gmail.com>
2018-10-02 12:32:13 -0500

    added process excercise

---
+++ excercises/global.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+printVersion = () => {
+    console.log(`Node.js version: ${process.version}`)
+    console.log(`v8 version: ${process.versions.v8}`)
+}
+
+printVersion()
+
+
+
+
