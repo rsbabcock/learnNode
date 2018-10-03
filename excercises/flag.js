aaa226a6b8567d0d1946446a4819a335ec1023a8
rsbabcock <rachael.s.babcock@gmail.com>
2018-10-03 14:51:08 -0500

    added flag excercise haven't done it yet

---
+++ excercises/flag.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+ const log = console.log
+
+log(chalk.blue('Hello world!'));
+log(chalk.blue.bgRed.bold(''));
--- excercises/global.js
+++ excercises/global.js
@@ -8,5 +8,10 @@ printVersion = () => {
 printVersion()
 
 
+const bold = string => `\u001b[1m${string}\u001b[22m`
+const green = string => `\u001b[32m${string}\u001b[39m`
+const red = string => `\u001b[31m${string}\u001b[39m`
+console.log(`It is ${red('easier')} to use ${green('functions for')} ${green(bold('ANSI'))} codes!`)
+
 
 
